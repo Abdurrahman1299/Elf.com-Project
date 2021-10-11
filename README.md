e07cd453fa293e38f5987a59eeed391c44fa04bd
Abdurrahman Muhammad <abdurrahmanms09@gmail.com>
2021-10-11 17:17:00 +0200

    third edition,, added new txt in the readme file and some changes with new feature in js file.

---
+++ README.md
@@ -0,0 +1,6 @@
+## in this readme txt file, i am going to explain the most important and newest things i've learned through this project.
+
+1. thing that was slightly hidden from me was the how to make the 'to top' button --> and I figured it out from zero web school.
+2. making the event listerner of clicking and showing the list was easy but with remembering some details from udacity course, i could solve it.
+3. remember to call the function after finishing its code. #call the function.
+4. adding and removing classes using classlist property is the only way to hid and reshow elements.
--- main.js
+++ main.js
@@ -17,3 +17,43 @@ button.onclick = function () {
 }
 // END MAKING A SCROLL TO TOP BUTTON
 
+
+// MAKING A LIST WITH THE TEARS TO CHOOSE FROM
+let list = function () {
+  let endYear = 1940;
+  let startYear = (new Date).getFullYear(); // current year
+  let selectedYear = "year";
+
+  let option = '';
+  option = '<option>-year-</option>'; // first option
+
+  for (let i = startYear; i >= endYear; --i) {
+    let selected = (i === selectedYear ? ' selected' : '');
+    option += '<option value="' + i + '"' + selected + '>' + i + '</option>';
+  }
+
+  document.getElementById("year").innerHTML = option;
+};
+
+document.querySelector('#year').addEventListener('click', list());
+
+// END MAKING A LIST WITH THE TEARS TO CHOOSE FROM
+
+
+// SHOWING THE DROP-LIST ON CLICKING THE (CHOOSEBY) BUTTON
+
+let dropList = document.getElementById('drop-list');
+let chooseBtn = document.getElementById('chooseBtn');
+
+let showList = function () {
+  chooseBtn.addEventListener('click', function one() {
+    if (dropList.classList.contains("drop-list-show")) {
+      dropList.classList.remove("drop-list-show")
+    } else {
+      dropList.classList.add("drop-list-show")
+    }
+  });
+}
+showList();
+
+// END SHOWING THE DROP-LIST ON CLICKING THE (CHOOSEBY) BUTTON
